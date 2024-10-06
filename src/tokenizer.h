25a0a209072cd3e5d90f7712b183dbbe43acc2f5
JosethValdez <jsvaldez2@miners.utep.edu>
2024-10-05 18:21:17 -0600

    Update tokenizer.h

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -5,20 +5,43 @@
 /* Return true (non-zero) if c is a whitespace characer
    ('\t' or ' ').  
    Zero terminators are not printable (therefore false) */
-int space_char(char c);
+int space_char(char c)
+{
+    if ((ch == ' ') || (ch == '\t'))
+        return 1;
+
+    return 0;
+}
 
 /* Return true (non-zero) if c is a non-whitespace 
    character (not tab or space).  
    Zero terminators are not printable (therefore false) */ 
-int non_space_char(char c);
+int non_space_char(char c)
+{
+   if ((ch != ' ') && (ch != '\t'))
+        return 1;
+
+    return 0;
+}
 
 /* Returns a pointer to the first character of the next 
    space-separated token in zero-terminated str.  Return a zero pointer if 
    str does not contain any tokens. */
-char *token_start(char *str); 
+
 
 /* Returns a pointer terminator char following *token */
-char *token_terminator(char *token);
+char *token_terminator(char *token)
+{
+    while (*token != '\0') {  
+        if (*token == ' '){
+            token += 1;
+            return token;
+        }
+        token++;  
+    }
+    *token = 0;
+    return token;
+}
 
 /* Counts the number of tokens in the string argument. */
 int count_tokens(char *str);
