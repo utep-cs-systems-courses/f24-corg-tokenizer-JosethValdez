c641f74a5f15359951495a207d8a66b032a7c68e
JosethValdez <jsvaldez2@miners.utep.edu>
2024-10-09 13:36:32 -0600

    Update tokenizer.h

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -80,10 +80,62 @@ char *copy_str(char *inStr, short len) {
      tokens[2] = "string" 
      tokens[3] = 0
 */
-char **tokenize(char* str);
+char** tokenize(char* a_str, const char a_delim)
+{   
+    char** result    = 0;
+    size_t count     = 0;
+    char* tmp        = a_str;
+    char* last_comma = 0;
+    char delim[2];
+    delim[0] = a_delim;
+    delim[1] = 0;
+
+    while (*tmp)
+    {
+        if (a_delim == *tmp)
+        {
+            count++;
+            last_comma = tmp;
+        }
+        tmp++;
+    }
+
+    count += last_comma < (a_str + strlen(a_str) - 1);
+    count++;
+
+    result = malloc(sizeof(char*) * count);
+
+    if (result)
+    {
+        size_t idx  = 0;
+        char* token = strtok(a_str, delim);
+
+        while (token)
+        {
+            assert(idx < count);
+            *(result + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+        assert(idx == count - 1);
+        *(result + idx) = '\0';
+    }
+    return result;
+}
 
 /* Prints all tokens. */
-void print_tokens(char **tokens);
+void print_tokens(char **tokens){
+    if (tokens)
+    {
+        int i;
+        for (i = 0; *(tokens + i); i++)
+        {
+            printf("%d.- %s\n", i+1, *(tokens + i));
+            free(*(tokens + i));
+        }
+        printf("%d.- %s\n", i+1, *(tokens + i));
+        free(tokens);
+    }
+}
 
 /* Frees all tokens and the vector containing themx. */
 void free_tokens(char **tokens);
